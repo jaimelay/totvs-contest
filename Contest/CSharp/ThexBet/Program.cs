e9634659017ca81fc73c9a90fc553f82b8a0d335
Jaime Lay <jaimelay1997@gmail.com>
2023-07-26 23:38:22 -0300

    template csharp ThexBet and ThexBooking

---
+++ Contest/CSharp/ThexBet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThexBet
+{
+    class Program
+    {
+        private static long Solve(int n, int m, List<List<int>> bets)
+        {
+            // Complete este metodo para resolver a questao ThexBet
+        }
+        
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToList();
+            var n = input[0];
+            var m = input[1];
+
+            var bets = new List<List<int>>();
+            
+            for (var i = 0; i < n; i++)
+            {
+                var bet = Console.ReadLine().Split(" ").Select(x => Convert.ToInt32(x)).ToList();
+                bets.Add(bet);
+            }
+            
+            Console.WriteLine(Solve(n, m, bets));
+        }
+    }
+}
---
+++ Contest/CSharp/ThexBooking/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThexBooking
+{
+    class Program
+    {
+        private static string Solve(int n, int q, int k, List<List<int>> rooms)
+        {
+            // Complete este metodo para resolver a questao ThexBooking
+        }
+        
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToList();
+            int n = input[0], q = input[1], k = input[2];
+
+            var rooms = new List<List<int>>();
+            for (var i = 0; i < n; i++)
+            {
+                rooms.Add(Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToList());
+            }
+            
+            Console.WriteLine(Solve(n, q, k, rooms));
+        }
+    }
+}
