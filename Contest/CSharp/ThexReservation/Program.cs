5ad551811ca83f0bbd06b7e01553d76478e2259d
Jaime Lay <jaimelay1997@gmail.com>
2023-07-27 10:11:46 -0300

    add template csharp thexreservation

---
+++ Contest/CSharp/ThexReservation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThexReservation
+{
+    class Program
+    {
+        private static void Solve(int n, int q, List<List<int>> rooms)
+        {
+            // Complete este metodo para resolver a questao ThexReservation
+        }
+        
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToList();
+            int n = input[0], q = input[1];
+            var rooms = new List<List<int>>();
+ 
+            for (var i = 0; i < n; i++)
+            {
+                var room = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToList();
+                rooms.Add(room);
+            }
+
+            Solve(n, q, rooms);
+        }
+    }
+}
