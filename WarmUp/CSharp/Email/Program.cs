4d0be8ea3d4e2f9e09188f0cbb79e55e827e1c8b
Jaime Lay <jaimelay1997@gmail.com>
2023-07-27 10:38:53 -0300

    add warmup csharp templates

---
+++ WarmUp/CSharp/Email/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Email
+{
+    class Program
+    {
+        static void Solve(int n, List<string> words)
+        {
+            // Complete este metodo para resolver a questao Email
+        }
+        
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var words = Console.ReadLine().Split(' ').ToList();
+            Solve(n, words);
+        }
+    }
+}
---
+++ WarmUp/CSharp/PullRequest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PullRequest
+{
+    class Program
+    {
+        static void Solve(int n, List<List<int>> reviewers)
+        {
+            // Complete este metodo para resolver a questao PullRequest
+        }
+        
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var reviewers = new List<List<int>>();
+ 
+            for (var i = 0; i < n; i++)
+            {
+                var reviewersList = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToList();
+                reviewers.Add(reviewersList);
+            }
+
+            Solve(n, reviewers);
+        }
+    }
+}
